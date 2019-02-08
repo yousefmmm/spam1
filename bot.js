0dd49ccfb4c9cb37db8ab3906bc7d68f04adb48b
yousefmmm <47262455+yousefmmm@users.noreply.github.com>
2019-02-08 19:21:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("543480397342703676")
 setInterval(function() {
-channel.send(``);
+channel.send(`احا احا يعمو`);
 }, 30)
 })
 
